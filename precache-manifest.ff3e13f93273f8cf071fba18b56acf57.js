3a98f22b844e384c80f714fe5fa110852d6df862
Andres Long <andreslong95@gmail.com>
2020-04-10 22:16:06 -0500

    Updates

---
+++ precache-manifest.ff3e13f93273f8cf071fba18b56acf57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324b158e0c5c19e7166d6629fda59c76",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4264df2e44ec9edc36c2",
+    "url": "/react-portfolio/static/css/main.569b7366.chunk.css"
+  },
+  {
+    "revision": "a9c4563d53aa03c67634",
+    "url": "/react-portfolio/static/js/2.1b79b50e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio/static/js/2.1b79b50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4264df2e44ec9edc36c2",
+    "url": "/react-portfolio/static/js/main.8b92cc7c.chunk.js"
+  },
+  {
+    "revision": "a229530a0c0d1ed130b6",
+    "url": "/react-portfolio/static/js/runtime-main.65d8297c.js"
+  },
+  {
+    "revision": "5cf14926e96087ee82dc0597bfd963ef",
+    "url": "/react-portfolio/static/media/AndresLong.5cf14926.jpg"
+  },
+  {
+    "revision": "1aea882f65446d20c8cea1c9a2f6403c",
+    "url": "/react-portfolio/static/media/Background-code.1aea882f.jpeg"
+  }
+]);
